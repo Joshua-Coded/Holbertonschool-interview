dae1edb0ab6fda62773253c8339d98952420d060
dev_josh <Alanajoshua535@gmail.com>
2022-11-28 00:22:33 +0100

    lists.h

---
+++ 0x01-insert_in_sorted_linked_list/lists.h
@@ -0,0 +1,30 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly lnked list
+ *@n: integer
+ *@next: points to the next node
+ *
+ *Description: singly lnked list node structure
+ *for personal work
+ */
+
+typedef struct listint_s
+{
+  int n;
+  struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+listint_t *insert_node(listint_t **head, int number);
+
+#endif /* LISTs_H */
